51e78fd8c53010f3f9d169c54e190ea06a63c8a5
BSuser <bs@BS1093s-Laptop.local>
2023-04-11 19:31:06 +0600

    Intial setup of the project

---
+++ MovieTrends/Controllers/MainViewController/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  MovieTrends
+//
+//  Created by BSuser on 11/4/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.title = "Main View"
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
